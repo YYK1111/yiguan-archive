b28159b4244d487fc16aaa3aac5aa3f9d1ae966e
Yingkai Yue <yingkaiyue8@gmail.com>
2026-08-06 21:24:56 +0800

    Redesign 衣冠藏卷 exploration platform

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["127.0.0.1", "localhost", "10.70.80.64"],
 };
 
 export default nextConfig;
